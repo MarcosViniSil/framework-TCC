3a9126e380853959ca867ca13b061c738111b9c1
MarcosViniSil <marcossilv203@gmail.com>
2026-08-22 19:57:56 -0300

    feat: convert josn/jsonl to dict

--- src/dataset/load_dataset.py
+++ src/dataset/load_dataset.py
@@ -1,9 +1,34 @@
+from pathlib import Path
+
 import yaml
+import json
+import logging
 
-# Lendo o arquivo
-with open('../../model.yaml', 'r', encoding='utf-8') as file:
-    dados = yaml.safe_load(file)
-    print(dados)
+logger = logging.getLogger(__name__)
 
-# Agora você pode acessar os campos
-print(dados['experiment']['name'])
+def _get_corpus_path(yaml_path) -> str:
+    with open(yaml_path, 'r', encoding='utf-8') as file:
+        data = yaml.safe_load(file)
+
+        return data['dataset']['path']
+
+def convert_corpus_to_dict(yaml_path) -> dict:
+    corpus_path = _get_corpus_path(yaml_path)
+
+    with open(corpus_path, 'r', encoding='utf-8') as json_data:
+        try:
+            if corpus_path.endswith(".json"):
+                dados = json.load(json_data)
+            else:
+                dados = [json.loads(linha) for linha in json_data if linha.strip()] 
+            print(dados)
+        except Exception as e:
+            logger.warning(
+                "the following error occurred when trying to convert json to dict: {}", str(e)
+            )
+            raise ValueError("An error occurred when trying to convert json to dict")
+
+
+convert_corpus_to_dict("./model.yaml")
+
+
--- src/validator/validate_yaml.py
+++ src/validator/validate_yaml.py
@@ -1,7 +1,5 @@
 """Validation of the experiment configuration YAML file."""
 
-from __future__ import annotations
-
 import json
 import logging
 from pathlib import Path
